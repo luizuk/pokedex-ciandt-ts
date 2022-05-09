4fc767692c847dbed06ac27601e87cd15c8e4a7d
luizuk <luizuksouza@gmail.com>
2022-05-09 19:13:35 -0300

    chore: install and test chackra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { pokedexRoutes } from "./navigation/routes";
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from "./global/theme";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {pokedexRoutes.map((route, key) => {
-          return <Route key={key} path={route.path} element={route.element} />;
-        })}
-      </Routes>
-    </Router>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Routes>
+          {pokedexRoutes.map((route, key) => {
+            return <Route key={key} path={route.path} element={route.element} />;
+          })}
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
 }
 
--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,8 +1,19 @@
+import { Box, Center, Flex, Square, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Pokedex = () => {
   return (
-    <div>Pokedex</div>
+    <Flex color='white'>
+      <Center w='100px' bg='green.500'>
+        <Text>Box 1</Text>
+      </Center>
+      <Square bg='blue.500' size='150px'>
+        <Text>Box 2</Text>
+      </Square>
+      <Box flex='1' bg='tomato'>
+        <Text>Box 3</Text>
+      </Box>
+    </Flex>
   )
 }
 
