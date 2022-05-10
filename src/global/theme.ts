6ee1a706e269ef8ee567c9af5d3769b0f57e6023
luizuk <luizuksouza@gmail.com>
2022-05-10 12:36:26 -0300

    feat: start in dark mode

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,6 +1,11 @@
 import { extendTheme } from '@chakra-ui/react'
 
 // 2. Extend the theme to include custom colors, fonts, etc
+const config = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
 const colors = {
   brand: {
     900: '#1a365d',
@@ -9,4 +14,4 @@ const colors = {
   },
 }
 
-export const theme = extendTheme({ colors })
+export const theme = extendTheme({ colors, config })
