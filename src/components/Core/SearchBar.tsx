938983f52a306fab7f48a0a7f00f36dea903b283
luizuk <luizuksouza@gmail.com>
2022-05-10 20:14:59 -0300

    refactor: simplify searchBar

--- src/components/Core/SearchBar.tsx
+++ src/components/Core/SearchBar.tsx
@@ -1,15 +1,12 @@
-import { Search2Icon } from '@chakra-ui/icons'
-import { Button, Image, Input, InputGroup, InputLeftElement, InputRightElement, Text } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
+import { Image, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 
 interface SearchBarTypes {
   liftSearchText: (text: string) => void;
-  onClick: () => void
 }
 
 const SearchBar = ({
-  liftSearchText,
-  onClick
+  liftSearchText
 }: SearchBarTypes) => {
   const [searchText, setSearchText] = useState("")
 
@@ -18,7 +15,7 @@ const SearchBar = ({
   }, [searchText])
 
   return (
-    <InputGroup size={'lg'} w={['100%', '100%', '49%']} mr={{ lg: 2 }}>
+    <InputGroup size={'lg'} w={'100%'} mr={{ lg: 2 }}>
       <InputLeftElement>
         <Image
           width={'30px'}
@@ -36,20 +33,6 @@ const SearchBar = ({
         focusBorderColor={'red.500'}
         color={'gray.300'}
       />
-      <InputRightElement width='100px' alignItems={'center'} p={2}>
-        <Button
-          bg={'red.300'}
-          height={'30px'}
-          p={2}
-          alignItems={'center'}
-          onClick={onClick}
-        >
-          <Text color='red.600' fontWeight={'bold'} mr={2}>Find</Text>
-          <Search2Icon
-            color={'red.600'}
-          />
-        </Button>
-      </InputRightElement>
     </InputGroup>
   )
 }
