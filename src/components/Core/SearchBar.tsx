3dd9839620cff5820169621f5f796798a55b1326
luizuk <luizuksouza@gmail.com>
2022-05-10 15:55:51 -0300

    feat: searchButton and state changes

--- src/components/Core/SearchBar.tsx
+++ src/components/Core/SearchBar.tsx
@@ -1,12 +1,24 @@
 import { Search2Icon } from '@chakra-ui/icons'
-import { Image, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
-import React from 'react'
+import { Button, Image, Input, InputGroup, InputLeftElement, InputRightElement, Text } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
 
+interface SearchBarTypes {
+  liftSearchText: (text: string) => void;
+  onClick: () => void
+}
 
+const SearchBar = ({
+  liftSearchText,
+  onClick
+}: SearchBarTypes) => {
+  const [searchText, setSearchText] = useState("")
 
-const SearchBar = () => {
+  useEffect(() => {
+    liftSearchText(searchText)
+  }, [searchText])
+
   return (
-    <InputGroup size='md' w={['100%', '100%', '50%']}>
+    <InputGroup size={'lg'} w={['100%', '100%', '49%']} mr={{ lg: 2 }}>
       <InputLeftElement>
         <Image
           width={'30px'}
@@ -15,16 +27,28 @@ const SearchBar = () => {
         />
       </InputLeftElement>
       <Input
+        size={'lg'}
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
         placeholder='Find Pokemon'
         variant={'filled'}
         borderColor={'red.600'}
         focusBorderColor={'red.500'}
         color={'gray.300'}
       />
-      <InputRightElement width='4.5rem'>
-        <Search2Icon
-          color={'red.600'}
-        />
+      <InputRightElement width='100px' alignItems={'center'} p={2}>
+        <Button
+          bg={'red.300'}
+          height={'30px'}
+          p={2}
+          alignItems={'center'}
+          onClick={onClick}
+        >
+          <Text color='red.600' fontWeight={'bold'} mr={2}>Find</Text>
+          <Search2Icon
+            color={'red.600'}
+          />
+        </Button>
       </InputRightElement>
     </InputGroup>
   )
