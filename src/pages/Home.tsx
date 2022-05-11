4c94a3f982ee3a2cefe340f4231a4eb5a7cecec7
luizuk <luizuksouza@gmail.com>
2022-05-11 10:53:02 -0300

    feat: home buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Button, SimpleGrid, Text } from '@chakra-ui/react'
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import { DataContext } from '../contexts/DataProvider'
 
 const Home = () => {
@@ -7,8 +8,56 @@ const Home = () => {
 
   console.log("allPokemons", allPokemons)
   return (
-    <Box width={'100%'} bg={'black'}>
-      teste
+    <Box
+      width={'100%'}
+      bg={'black'}
+      p={2}
+      justifyContent={'center'}
+    >
+      <Link
+        to={'/search'}
+      >
+        <Button
+          colorScheme={'facebook'}
+          mr={2}
+        >
+          <Text
+            textAlign={'center'}
+          >
+            Search By Name
+          </Text>
+        </Button>
+      </Link>
+
+      <Link
+        to={'/filters'}
+      >
+        <Button
+          colorScheme={'facebook'}
+          mr={2}
+        >
+          <Text
+            textAlign={'center'}
+          >
+            Filters
+          </Text>
+        </Button>
+      </Link>
+
+
+      <Link
+        to={'/favorites'}
+      >
+        <Button
+          colorScheme={'facebook'}
+        >
+          <Text
+            textAlign={'center'}
+          >
+            Favorites
+          </Text>
+        </Button>
+      </Link>
     </Box>
   )
 }
