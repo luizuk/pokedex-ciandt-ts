fb78de9911d15bc34f425466fdae6c5ae4519965
luizuk <luizuksouza@gmail.com>
2022-05-11 11:43:59 -0300

    feat: favorites context start implement

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { Box, Flex, SimpleGrid, Text } from '@chakra-ui/react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { DataContext } from '../contexts/DataProvider'
 
 const Favorites = () => {
+  const { favorites, setFavorites }: any = useContext(DataContext)
+
   return (
-    <div>Favorites</div>
+    <Box p={4}>
+      <Text
+        fontSize={22}
+        fontWeight={'bold'}
+      >
+        Favorites Pokemons
+      </Text>
+
+      <SimpleGrid columns={[1, 2, 3]} spacing='20px' mt={2} >
+        {
+          favorites.map((pokemon: any, index: number) => (
+            <Link
+              to={`/${pokemon.pokemon.name}`}
+              state={{ url: pokemon.pokemon.url }}
+              key={index}
+            >
+              <Flex
+                bg='red.700'
+                height={'60px'}
+                justifyContent={'center'}
+                alignItems={'center'}
+              >
+                <Text textAlign={'center'} fontSize={'1.5rem'}>
+                  {pokemon.pokemon.name}
+                </Text>
+              </Flex>
+            </Link>
+          ))
+        }
+      </SimpleGrid>
+    </Box>
   )
 }
 
--- src/pages/SpecificPokemon.tsx
+++ src/pages/SpecificPokemon.tsx
@@ -1,13 +1,15 @@
-import { Avatar, Flex, Image, Text } from "@chakra-ui/react";
+import { Avatar, Button, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { DataContext } from "../contexts/DataProvider";
 
 // interface urlProp {
 //   url: string
 // }
 
 const SpecificPokemon = () => {
+  const { favorites, setFavorites }: any = useContext(DataContext)
   const location = useLocation();
   const { url }: any = location.state;
   console.log('url', url)
@@ -23,25 +25,36 @@ const SpecificPokemon = () => {
     getSpecificPokemon()
   }, [])
 
+  const addToFavorites = () => {
+    const favoriteAdded = [...favorites]
+    favoriteAdded.push(...favorites, specificPokemonData)
+    setFavorites(favoriteAdded)
+  }
+
   if (!specificPokemonData) return null
 
-  console.log('specificPokemonData', specificPokemonData)
+  console.log('favorites', favorites)
+  // console.log('specificPokemonData', specificPokemonData)
 
   return (
     <Flex
       w={'100%'}
       bg={'black'}
+      flexDirection={['column', 'column', 'row']}
+      alignItems={{
+        md: 'center', lg: 'center'
+      }}
       p={2}
     >
       <Flex
-        w={'300px'}
-        h={'300px'}
+        w={['100%', '100%', '300px']}
         bg={'red.600'}
         rounded={'lg'}
         p={4}
         flexDirection={'column'}
         alignItems={'center'}
       >
+
         <Text
           fontSize={32}
           fontWeight={'bold'}
@@ -49,16 +62,96 @@ const SpecificPokemon = () => {
           {specificPokemonData.name.toUpperCase()}
         </Text>
 
+        {
+          specificPokemonData.types.map((type: any, index: number) => (
+            <Text
+              key={index}
+              fontWeight={"bold"}
+              color={'gray.300'}
+            >
+              {`Type ${type.slot}: (${type.type.name})`}
+            </Text>
+          ))
+        }
+
         <Avatar
           bg={'yellow.100'}
-          w={200}
-          h={200}
+          w={150}
+          h={150}
           src={specificPokemonData.sprites.front_default}
         />
 
+        {
+          !(favorites.find((name: string) => name === specificPokemonData.name)) &&
+          <Button
+            my={2}
+            bg={"gray.800"}
+            onClick={() => addToFavorites()}
+          >
+            Favorite ⭐
+          </Button>
+        }
+
       </Flex>
 
-    </Flex>
+      <Flex
+        w={['100%', '100%', '60%']}
+        mt={[2, 2, 0]}
+        ml={{ md: 4 }}
+        bg={'gray.800'}
+        rounded={'lg'}
+        p={4}
+        flexDirection={'column'}
+
+      >
+        <Text
+          fontSize={'lg'}
+          fontWeight={'bold'}
+        >
+          {'Stats Cards'.toUpperCase()}
+        </Text>
+
+        <SimpleGrid
+          columns={3}
+          spacing='30px'
+          mt={2}
+        >
+          {
+            specificPokemonData.stats.map((stats: any, index: number) => (
+              <Flex
+                key={index}
+                w={'120px'}
+                h={'120px'}
+                bg={'purple.700'}
+                rounded={'lg'}
+                alignItems={'center'}
+                justifyContent={'flex-end'}
+                paddingY={5}
+                flexDirection={'column'}
+              >
+                <Text
+                  textAlign={'center'}
+                  fontSize={18}
+                  fontWeight={'bold'}
+                  color={'yellow.400'}
+                >
+                  {stats.stat.name.toUpperCase()}
+                </Text>
+
+                <Text
+                  textAlign={'center'}
+                  fontSize={26}
+                  fontWeight={'bold'}
+                >
+                  {stats.base_stat}
+                </Text>
+              </Flex>
+            ))
+          }
+        </SimpleGrid>
+      </Flex>
+
+    </Flex >
   )
 }
 
